1a69fd633e0550040a02c2bd701f67d611e9d151
Dakto <dakotakearneycole@outlook.com>
2025-07-16 15:39:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Directory created at ~/RPS"
 sleep 0.5
 echo "Downloading python script..."
 sleep 1
-cd ~/RPS && wget https://raw.githubusercontent.com/Daktoo/rock-paper-scissors/refs/heads/master/rps.py
+cd ~/RPS && wget https://raw.githubusercontent.com/Daktoo/rock-paper-scissors/refs/heads/master/rps.py >> /dev/null 2>&1
 sleep 1
 echo "Python script downloaded successfully."
 sleep 0.5
